62897e8e13e87fc7b5f2b6f902c9368c91c140a8
Pranjal-ux <pranjal2shukla@gmail.com>
2026-02-17 19:27:51 +0530

    #Changes

--- Lec11/src/app.js
+++ Lec11/src/app.js
@@ -9,9 +9,18 @@ app.post('/notes', (req, res) => {
     notes.push(req.body)
 
     res.status(201).json({
-        message: "notes created successfully"
+        message: "notes created successfully "
     })
 
 })
+
+//Get api
+app.get('/notes',((req,res)=>{
+res.status(200).json({
+    message:"notes fetch  succssfully",
+    notes:notes
+})
+
+}))
 
 module.exports = app

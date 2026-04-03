d268eb3c45dec683a6e5934b7315622f9f369c66
Pranjal-ux <pranjal2shukla@gmail.com>
2026-04-03 18:53:13 +0530

    added multer middleware

--- postApp/src/app.js
+++ postApp/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express();
+const db = require("./db/post.db")
+const postModel = require("./models/post.model")
 
-
-app.get("/", (req, res) => {
-    res.send("hello")
+app.use(express.json())
+app.post("/create-post",async(req,res)=>{
+    console.log(req.body)
 })
-
 
 module.exports = app;
--- postApp/src/models/post.model.js
+++ postApp/src/models/post.model.js
@@ -4,3 +4,6 @@ const postSchema = new mongoose.Schema({
     image: String,
     caption: String
 })
+const postModel = mongoose.model("post", postSchema);
+
+module.exports = postModel;

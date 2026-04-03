41a08384feef7dd55bdbee4d15e5481fa85d094e
Pranjal-ux <pranjal2shukla@gmail.com>
2026-04-03 20:03:26 +0530

    ImgaKIT

--- postApp/src/app.js
+++ postApp/src/app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express();
 const db = require("./db/post.db")
 const postModel = require("./models/post.model")
+const multer = require('multer')
+
+const upload = multer({ storage: multer.memoryStorage() })
 
 app.use(express.json())
-app.post("/create-post",async(req,res)=>{
+app.post("/create-post", upload.single("image"), async (req, res) => {
     console.log(req.body)
+    console.log(req.file)
 })
 
 module.exports = app;

d1ea64ea9c6e6e781ae474115c7dc4df56b1f8db
Pranjal-ux <pranjal2shukla@gmail.com>
2026-04-04 15:49:35 +0530

    #Changes

--- postApp/src/app.js
+++ postApp/src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const db = require("./db/post.db")
 const postModel = require("./models/post.model")
 const multer = require('multer')
+const uploadFile = require('./services/storage')
 
 const upload = multer({ storage: multer.memoryStorage() })
 
@@ -10,6 +11,8 @@ app.use(express.json())
 app.post("/create-post", upload.single("image"), async (req, res) => {
     console.log(req.body)
     console.log(req.file)
+    const result = await uploadFile(req.file.buffer)
+    console.log(result)
 })
 
 module.exports = app;
--- postApp/src/services/storage.js
+++ postApp/src/services/storage.js
@@ -1,5 +1,16 @@
-const Imagekit = require("@imagekit/nodejs")
+const ImageKit = require("@imagekit/nodejs")
 
-const imagekit = new Imagekit({
-    privateKey: "private_SgRKId3pHt2EGPCWQxiudV + rHfs="
-})  
+const imagekit = new ImageKit({
+    publicKey: "public_zpIvVaq/2ikqEkHygoo64Kd9bSM=",
+    privateKey: "private_SgRKId3pHt2EGPCWQxiudV+rHfs=",
+
+})
+
+async function uploadFile(buffer) {
+    const result = await imagekit.upload({
+        file: buffer,
+        fileName: "image.jpg"
+    })
+    return result
+}
+module.exports = uploadFile;

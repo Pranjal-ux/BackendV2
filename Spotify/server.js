0d75b18d7096a129ca1e1b31509a19607fdfdaee
Pranjal-ux <pranjal2shukla@gmail.com>
2026-04-07 12:36:24 +0530

    #create model folder

--- Spotify/server.js
+++ Spotify/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-
 const app = require('./src/app')
-app.listen(3000,()=>{
+const db = require("./src/db/database")
+db();
+app.listen(3000, () => {
     console.log("Server is running")
 })
--- Spotify/src/app.js
+++ Spotify/src/app.js
@@ -1,12 +1,16 @@
 const cookieParser = require('cookie-parser');
-const express =  require('express')
+const express = require('express')
 const app = express();
+const userModel = require('./models/userModel')
 app.use(express.json())
 app.use(cookieParser())
 
 
-app.get('/',(req,res)=>{
-res.send("hello")
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+const user = userModel.create({
+    
 })
 
 

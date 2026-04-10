e4d6ab60207c196b8ee05397f33484b89226e93c
Pranjal-ux <pranjal2shukla@gmail.com>
2026-04-10 20:10:48 +0530

    #Login feature done

--- Spotify/src/controllers/authController.js
+++ Spotify/src/controllers/authController.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 const bcrypt = require("bcryptjs")
+
+//Register controller.....
 async function registerUser(req, res) {
 
     const { username, email, password, role = "user" } = req.body;
@@ -41,5 +43,42 @@ async function registerUser(req, res) {
         }
     })
 }
+// loging Controller...
+async function loginUser(req, res) {
+    const { username, email, password } = req.body
+    const user = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+   
+    if (!user) {
+        return res.staus(401).json({
+            message: "User not exist"
+        })
+    }
+    const ispasswordValid = await bcrypt.compare(password, user.password)
+    if (!ispasswordValid) {
+        return res.status(401).send("Wrong Password")
+    }
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role
+    }, process.env.JWT_SECRET)
 
-module.exports = { registerUser }
+    res.cookie("token", token)
+    res.status(200).json({
+        message: "User Logged in sucessfully",
+        user: {
+            id: user._id,
+            email: user.email,
+            password: user.password,
+            role: user.role
+
+
+        }
+    })
+}
+
+module.exports = { registerUser, loginUser }
--- Spotify/src/routers/authRoute.js
+++ Spotify/src/routers/authRoute.js
@@ -2,5 +2,6 @@ const express = require('express')
 const authController = require('../controllers/authController')
 const router = express.Router()
 router.post('/register', authController.registerUser)
+router.post('/login', authController.loginUser)
 
 module.exports = router;

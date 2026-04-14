4e76651b90514e411e0aba971eb3ddf3b0e3bf69
Pranjal-ux <pranjal2shukla@gmail.com>
2026-04-14 19:37:36 +0530

    ImageKit

--- Spotify/src/controllers/musicController.js
+++ Spotify/src/controllers/musicController.js
@@ -1,1 +1,19 @@
 const artistModel = require('../models/artistModel')
+const jwt = require("jsonwebtoken")
+
+async function createMusic(req, res) {
+
+    const token = req.cookie.token;
+    if (!token) {
+        return res.status(401).json({message:"Unauthorised user"});
+    }
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+           if(decode.role!="artist"){
+            return res.status(401).json({message:"you don't have access to create music"})
+           }
+    } catch (err) {
+        return res.status(401).json({})
+    }
+
+}

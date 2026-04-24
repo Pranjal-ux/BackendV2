ba9f7735edeb10d5bdaf2c9a85d70625859d3641
Pranjal-ux <pranjal2shukla@gmail.com>
2026-04-24 20:37:53 +0530

    ..

--- DevRoom/app.js
+++ DevRoom/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express();
-app.use((req, res) => {
+app.use("/hello", (req, res) => {
+    res.send("hello form america!!!")
+})
+app.use("/hello/nihao", (req, res) => {
+    res.send("Hello! from japan!!!")
+})
+app.use("/", (req, res) => {
     res.send("Hello! from server")
 })
 app.listen(3000, (req, res) => {

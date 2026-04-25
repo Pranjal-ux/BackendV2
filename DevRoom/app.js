6f2495a67ec8bb3e2a009364395495430e19539d
Pranjal-ux <pranjal2shukla@gmail.com>
2026-04-25 17:11:19 +0530

    Multiple-route-handler

--- DevRoom/app.js
+++ DevRoom/app.js
@@ -6,8 +6,14 @@ app.use("/hello", (req, res) => {
 app.use("/hello/nihao", (req, res) => {
     res.send("Hello! from japan!!!")
 })
-app.use("/", (req, res) => {
-    res.send("Hello! from server")
+app.get("/user", (req, res,next) => {
+    console.log("Route handler 1....")
+    //  res.send("Hello! from user")
+    next();
+
+},(req,res)=>{
+    console.log("Route handler 2....")
+    res.send("Hello! from user 2 ....")
 })
 app.listen(3000, (req, res) => {
     console.log("Server is running on port 3000.... ")
